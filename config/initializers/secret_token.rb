62fb2ec13154c2dc7bbd229f662fe89171c55b23
sanemat <o.gata.ken@gmail.com>
2013-01-12 23:38:02 +0900

    New rails
    $ bundle exec rails new . -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kagari::Application.config.secret_key_base = '5e58d74bb76a1f36a2ac2de9d8586bbfbb57497852c6fa514fb42dc9933c6a6fb781f769c6430f9ba08024394274dcc5c598dca37aabca4c9ee26a9173e894fe'
